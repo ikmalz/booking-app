7b7ae53c1a37e181782cd854fc91655bd225e7f1
ikmalz <ikmalfairuz08@gmail.com>
2025-11-28 19:54:54 +0700

    fix: all

--- app/checkout/[id]/page.tsx
+++ app/checkout/[id]/page.tsx
@@ -7,19 +7,23 @@ export const metadata: Metadata = {
   title: "Reservation Summary",
 };
 
-const CheckoutPage = async ({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) => {
-  const reservationId = (await params).id;
+interface CheckoutPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const CheckoutPage = async ({ params }: CheckoutPageProps) => {
+  const reservationId = params.id;
 
   return (
     <div className="max-w-screen-xl px-4 mx-auto py-20 mt-12">
       <h1 className="text-2xl font-semibold mb-8">Reservation Summary</h1>
+
       <Suspense fallback={<p>Loading...</p>}>
         <CheckoutDetail reservationId={reservationId} />
       </Suspense>
+
       <Script
         src="https://app.sandbox.midtrans.com/snap/snap.js"
         data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY}
